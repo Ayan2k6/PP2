2880c919026886bf9b5386c2a9e002ecc9d6b1f1
Ayan2k6 <ay_saken@kbtu.kz>
2025-01-17 17:11:09 +0500

    DataTypes

---
+++ LAB1/task5.py
@@ -0,0 +1,31 @@
+q = "Hello World"
+w = 20
+e = 20.5
+r = 1j
+t = ["apple", "banana", "cherry"]
+y = ("apple", "banana", "cherry")
+u = range(6)
+i = {"name" : "John", "age" : 36}
+o = {"apple", "banana", "cherry"}
+p = frozenset({"apple", "banana", "cherry"})
+a = True
+s = b"Hello"
+d = bytearray(5)
+f = memoryview(bytes(5))
+g = None
+
+print(type(q))
+print(type(w))
+print(type(e))
+print(type(r))
+print(type(t))
+print(type(y))
+print(type(u))
+print(type(i))
+print(type(o))
+print(type(p))
+print(type(a))
+print(type(s))
+print(type(d))
+print(type(f))
+print(type(g))
