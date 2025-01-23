18ec747d1afcc924e689eafebef1c2728df89f67
Ayan2k6 <ay_saken@kbtu.kz>
2025-01-23 21:51:13 +0500

    Tuple

--- LAB2/task4.py
+++ LAB2/task4.py
@@ -90,3 +90,55 @@ thistuple = tuple(y)
 thistuple = ("apple", "banana", "cherry")
 del thistuple
 # print(thistuple) #this will raise an error because the tuple no longer exists
+
+
+# Unpack Tuples
+# Unpacking a Tuple
+fruits = ("apple", "banana", "cherry")
+
+(green, yellow, red) = fruits
+print(green)
+print(yellow)
+print(red)
+
+# Using Asterisk
+fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+(green, yellow, *red) = fruits
+print(green)
+print(yellow)
+print(red)
+
+fruits = ("apple", "mango", "papaya", "pineapple", "cherry")
+(green, *tropic, red) = fruits
+print(green)
+print(tropic)
+print(red)
+
+
+# Loop Tuples
+# Loop Through a Tuple
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+  print(x)
+
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+  print(thistuple[i])
+
+thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+  print(thistuple[i])
+  i = i + 1
+
+# Join Tuples
+# Join Two Tuples
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+# Multiply Tuples
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+print(mytuple)
