97f08e15042638e108356cc42d43bfec2dbed6e9
Ayan2k6 <ay_saken@kbtu.kz>
2025-02-01 15:55:48 +0500

    among the chickens and rabbits

--- LAB3/FUNCTION1/task3.py
+++ LAB3/FUNCTION1/task3.py
@@ -1,16 +1,16 @@
-def myfunc(numheads, numlegs):
+def solve(numheads, numlegs):
     # Let x be the number of chickens and y be the number of rabbits
     # We have two equations:
     # x + y = numheads  (total number of animals)
-    # 2x + 4y = numlegs (total number of legs)
+    # 2x + 4y = numlegs (total number of legs)  --> x + 2y = numlegs / 2
 
     # Solving for x and y:
-    x = numlegs - 2 * y
-    y = numheads - x
+    y = (numlegs // 2) - numheads
+    x = numheads - y
 
-    return x,y
+    return f"We have {x} chickens and {y} rabbits"
 print ("numer of heads: ")
-y = int(input())
+numheads = int(input())
 print ("numer of legs: ")
-x = int(input())
-print(myfunc(numheads, numlegs))
+numlegs = int(input())
+print(solve(numheads, numlegs))
